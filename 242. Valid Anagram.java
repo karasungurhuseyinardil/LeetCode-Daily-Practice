1eee6ea4f1a81ab0c687656f7b1e90fe932c60d1
Hüseyin Ardıl Karasungur <72706704+karasungurhuseyinardil@users.noreply.github.com>
2024-07-28 22:37:14 +0300

    Update 242. Valid Anagram.java

--- 242. Valid Anagram.java
+++ 242. Valid Anagram.java
@@ -1,1 +1,29 @@
-
+class Solution {
+    public boolean isAnagram(String s, String t) {
+       
+        if (s.length() != t.length()) {
+            return false; 
+        }
+      
+    
+        int[] letterCount = new int[26];
+      
+       
+        for (int i = 0; i < s.length(); i++) {
+            
+            letterCount[s.charAt(i) - 'a']++;
+            
+            letterCount[t.charAt(i) - 'a']--;
+        }
+      
+        
+        for (int count : letterCount) {
+            if (count != 0) {
+                return false; 
+            }
+        }
+      
+        
+        return true;
+    }
+}
